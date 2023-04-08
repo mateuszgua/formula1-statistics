7eee5f086d866023ab644b977b128d69d01dc76d
Mateusz Gula <mateuszgua@wp.eu>
2023-04-08 13:58:58 +0200

    Refactor s3 client

--- app/get_s3bucket_client.py
+++ app/get_s3bucket_client.py
@@ -1,23 +1,16 @@
-import csv
 import boto3
 
 from config import Config
 
 
-config = Config
+def get_client_s3():
+    config = Config
 
-s3 = boto3.client(
-    's3',
-    aws_access_key_id=config.AWS_KEY_ID,
-    aws_secret_access_key=config.AWS_ACCESS_KEY,
-    region_name=config.AWS_REGION_NAME
-)
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=config.AWS_KEY_ID,
+        aws_secret_access_key=config.AWS_ACCESS_KEY,
+        region_name=config.AWS_REGION_NAME
+    )
 
-obj = s3.get_object(Bucket=config.AWS_BUCKET,
-                    Key='data/source/circuits.csv')
-data = obj['Body'].read().decode('utf-8').splitlines()
-records = csv.reader(data)
-headers = next(records)
-print('headers: %s' % (headers))
-for eachRecord in records:
-    print(eachRecord)
+    return s3
