eb84e27be06ba8c32a890d211f8fb60b25c0aa11
Mateusz Gula <mateuszgua@wp.eu>
2023-04-25 20:50:34 +0200

    Feat: add create database if not exist

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 from app.get_object_from_s3 import get_object_from_s3
 from my_database import MyDatabase
+from database_manager import DatabaseManager
 
 file_path = 'data/source/circuits.csv'
 
-# records = get_object_from_s3(file_path)
-# headers = next(records)
+records = get_object_from_s3(file_path)
+headers = next(records)
 
-# print('headers: %s' % (headers))
-# for eachRecord in records:
-#     print(eachRecord)
-
+print('headers: %s' % (headers))
+for eachRecord in records:
+    print(eachRecord)
 
 db = MyDatabase()
 db.get_connection()
+
+manager = DatabaseManager()
+manager.create_database()
