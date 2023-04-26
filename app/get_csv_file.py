8ebf8d13bc54c297f1bccd22fbb4eaadf3a3b928
Mateusz Gula <mateuszgua@wp.eu>
2023-04-26 18:15:42 +0200

    Feat: update pyspark reader

--- app/get_csv_file.py
+++ app/get_csv_file.py
@@ -5,11 +5,12 @@ def get_csv_file(path_csv):
     spark = SparkSession.builder.getOrCreate()
     print(spark)
 
-    spark_df_a = spark.read \
-        .option("header", False) \
-        .option("sep", ",") \
-        .option("inferSchema", True) \
-        .csv(path=f'{path_csv}')
+    df = spark.read.format("csv").option("header", "true").option(
+        "inferSchema", "true").load(path_csv)
+    df.show()
 
-    spark_df_a.printSchema()
-    spark_df_a.describe().show()
+    # spark_df_a = spark.read.option("header", True).option(
+    #     "sep", ",").option("inferSchema", True).csv(path=f'{path_csv}')
+
+    # spark_df_a.printSchema()
+    # spark_df_a.describe().show()
