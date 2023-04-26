45e07a02a7c4f5f482833bfb9b89d1296bfa9a4a
Mateusz Gula <mateuszgua@wp.eu>
2023-04-26 18:16:04 +0200

    Feat: update main

--- app/main.py
+++ app/main.py
@@ -1,18 +1,22 @@
 from app.get_object_from_s3 import get_object_from_s3
 from my_database import MyDatabase
 from database_manager import DatabaseManager
+from get_csv_file import get_csv_file
 
 file_path = 'data/source/circuits.csv'
 
 records = get_object_from_s3(file_path)
 headers = next(records)
 
-print('headers: %s' % (headers))
-for eachRecord in records:
-    print(eachRecord)
+# print(type(records))
+# print('headers: %s' % (headers))
+# for eachRecord in records:
+#     print(eachRecord)
 
-db = MyDatabase()
-db.get_connection()
+get_csv_file(records)
 
-manager = DatabaseManager()
-manager.create_database()
+# db = MyDatabase()
+# db.get_connection()
+
+# manager = DatabaseManager()
+# manager.create_database()
