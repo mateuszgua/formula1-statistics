ff1f0566b5bbc6ba8da1a4a921c8e2075d123f11
Mateusz Gula <mateuszgua@wp.eu>
2023-04-27 18:14:43 +0200

    Refactor: change return value from function from object to list

---
+++ app/get_list_from_s3.py
@@ -0,0 +1,18 @@
+import csv
+
+from get_s3bucket_client import get_client_s3
+from config import Config
+
+
+def get_list_from_s3(file_path):
+    s3 = get_client_s3()
+    obj = s3.get_object(Bucket=Config.AWS_BUCKET,
+                        Key=file_path)
+    data = obj['Body'].read().decode('utf-8').splitlines()
+    records = csv.reader(data)
+    headers = next(records)
+    csvData = []
+    csvHeaders = headers
+    for eachRecord in records:
+        csvData.append(eachRecord)
+    return csvHeaders, csvData
