6f76ede97fac0fe9fee9c9249318f6a8813b37cc
Mateusz Gula <mateuszgua@wp.eu>
2023-05-01 10:59:45 +0200

    Feat: change call function file

--- app/transform_datas.py
+++ app/transform_datas.py
@@ -1,2 +1,30 @@
-
+from get_list_from_s3 import get_list_from_s3
+from get_df_spark import get_df_spark
 # get data from S3 create dataframe in other file and transform data
+
+file_path = 'data/source/circuits.csv'
+file_path_1 = 'data/source/constructor_results.csv'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+file_path_ = 'data/source/'
+
+header, data = get_list_from_s3(file_path)
+# headers = next(records)
+
+# print(type(records))
+# print('headers: %s' % (headers))
+# for eachRecord in records:
+#     print(eachRecord)
+
+dataframe = get_df_spark(header, data)
+dataframe.show()
