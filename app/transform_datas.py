225fab8778b733929a5ed0c2842630260d62b2a6
Mateusz Gula <mateuszgua@wp.eu>
2023-05-02 12:12:38 +0200

    Feat: add file paths

--- app/transform_datas.py
+++ app/transform_datas.py
@@ -2,23 +2,22 @@ from get_list_from_s3 import get_list_from_s3
 from get_df_spark import get_df_spark
 # get data from S3 create dataframe in other file and transform data
 
-file_path = 'data/source/circuits.csv'
-file_path_1 = 'data/source/constructor_results.csv'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
-file_path_ = 'data/source/'
+file_path_circuits = 'data/source/circuits.csv'
+file_path_constructor_results = 'data/source/constructor_results.csv'
+file_path_constructor_standings = 'data/source/constructor_standings.csv'
+file_path_constructors = 'data/source/constructors.csv'
+file_path_driver_standings = 'data/source/driver_standings.csv'
+file_path_drivers = 'data/source/drivers.csv'
+file_path_lap_times = 'data/source/lap_times.csv'
+file_path_pit_stops = 'data/source/pit_stops.csv'
+file_path_qualifying = 'data/source/qualifying.csv'
+file_path_races = 'data/source/races.csv'
+file_path_results = 'data/source/results.csv'
+file_path_seasons = 'data/source/seasons.csv'
+file_path_sprint_results = 'data/source/sprint_results.csv'
+file_path_status = 'data/source/status.csv'
 
-header, data = get_list_from_s3(file_path)
+header, data = get_list_from_s3(file_path_circuits)
 # headers = next(records)
 
 # print(type(records))
