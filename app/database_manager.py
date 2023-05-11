07059aec59855330ab945123a0ecf93b40e96dd2
Mateusz Gula <mateuszgua@wp.eu>
2023-05-11 13:21:44 +0200

    Feat: add function for check if database and tables exist

--- app/database_manager.py
+++ app/database_manager.py
@@ -1,3 +1,5 @@
+import pyodbc
+
 from config import Config
 from my_database import MyDatabase
 
@@ -8,23 +10,61 @@ class DatabaseManager:
         self.db = MyDatabase()
         self.cursor = self.db.get_connection()
 
+    def is_database_exist(self):
+        try:
+            sql_exist = f"""SELECT 1 FROM sys.databases WHERE name='{Config.DB_NEW_NAME}'"""
+            self.cursor.execute(sql_exist)
+            result = self.cursor.fetchone()
+
+            if result[0] is None:
+                self.create_database()
+
+        except pyodbc.Error as e:
+            print(f"There is a problem with database connection: {e}")
+
+    def is_table_exist(self, table_name):
+        try:
+            sql_exist = f"""SELECT OBJECT_ID(N'[{Config.DB_NEW_NAME}].[dbo].[{table_name}]', N'U')"""
+            self.cursor.execute(sql_exist)
+            result = self.cursor.fetchone()
+
+            if result[0] is None:
+                self.create_tables(table_name)
+
+        except pyodbc.Error as e:
+            print(f"There is a problem with database connection: {e}")
+
     def create_database(self):
         try:
-            sql_create = f"""IF NOT EXISTS(SELECT 1 FROM sys.databases WHERE name='{Config.DB_NEW_NAME}') CREATE DATABASE {Config.DB_NEW_NAME}"""
+            sql_create = f"""IF NOT EXISTS(SELECT 1 FROM sys.databases WHERE name='{Config.DB_NEW_NAME}') 
+            CREATE DATABASE {Config.DB_NEW_NAME} USE {Config.DB_NEW_NAME}"""
             self.cursor.execute(sql_create)
-        except:
-            print(f"There is a problem with create database")
+        except pyodbc.Error as e:
+            print(f"There is a problem with create database: {e}")
         else:
             print("Database created successfully.")
 
-    def create_tables(self):
+    def create_tables(self, table_name):
         try:
-            pass
-
-            # sql_create_table = (
-            # f"""IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='tbl_name' and xtype='U') CREATE TABLE tbl_name (Name varchar(64) not null)""")
-            # self.db.sql_execute(sql_create)
-        except:
-            pass
-        finally:
+            table_name = None
+            match table_name:
+                case "circuits":
+                    sql_create_table_circuits = (
+                        f"""IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[circuits]') AND type in (N'U'))
+                        CREATE TABLE circuits (
+                                circuitId int NOT NULL PRIMARY KEY,
+                                circuitRef varchar(64) NOT NULL,
+                                name varchar(100) NOT NULL,
+                                location varchar(64) NOT NULL,
+                                country varchar(64) NOT NULL,
+                                lat float(5) NOT NULL,
+                                lng float(5) NOT NULL,
+                                alt int
+                                )""")
+                case "":
+                    pass
+            self.cursor.execute(sql_create_table_circuits)
+        except pyodbc.Error as e:
+            print(f"There is a problem with create tables: {e}")
+        else:
             print("Tables created successfully.")
