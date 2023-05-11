3558f369f22f3daab7341eb8f069dbc88bdef4c5
Mateusz Gula <mateuszgua@wp.eu>
2023-05-11 20:29:03 +0200

    Feat: add error handlers for spark functions

--- app/get_list_from_s3.py
+++ app/get_list_from_s3.py
@@ -5,14 +5,18 @@ from config import Config
 
 
 def get_list_from_s3(file_path):
-    s3 = get_client_s3()
-    obj = s3.get_object(Bucket=Config.AWS_BUCKET,
-                        Key=file_path)
-    data = obj['Body'].read().decode('utf-8').splitlines()
-    records = csv.reader(data)
-    headers = next(records)
-    csvData = []
-    csvHeaders = headers
-    for eachRecord in records:
-        csvData.append(eachRecord)
-    return csvHeaders, csvData
+    try:
+        s3 = get_client_s3()
+        obj = s3.get_object(Bucket=Config.AWS_BUCKET,
+                            Key=file_path)
+        data = obj['Body'].read().decode('utf-8').splitlines()
+        records = csv.reader(data)
+        headers = next(records)
+        csvData = []
+        csvHeaders = headers
+        for eachRecord in records:
+            csvData.append(eachRecord)
+    except:
+        print(f"Error with get list from s3 bucket")
+    else:
+        return csvHeaders, csvData
